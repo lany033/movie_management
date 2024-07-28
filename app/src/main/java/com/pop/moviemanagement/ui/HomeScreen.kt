a9200faab1a2ae1c5ce9aa6b308b48a1cb0f6ddf
Melanie <mmantilla930@gmail.com>
2024-07-28 18:45:38 -0500

    carrousel in Home Screen v1.0

--- app/src/main/java/com/pop/moviemanagement/ui/HomeScreen.kt
+++ app/src/main/java/com/pop/moviemanagement/ui/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,21 +20,18 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Logout
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.ExitToApp
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -41,27 +39,35 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.google.firebase.auth.FirebaseUser
 import com.pop.moviemanagement.R
+import com.pop.moviemanagement.data.Movie
+import com.pop.moviemanagement.model.repository.MovieRepository
 import com.pop.moviemanagement.ui.navigation.AuthGraph
 import com.pop.moviemanagement.ui.navigation.BottomBarNavItem
-import com.pop.moviemanagement.ui.navigation.TrackScreen
-import com.pop.moviemanagement.ui.theme.PurpleGrey40
-import com.pop.moviemanagement.utils.AnalyticsManager
 import com.pop.moviemanagement.utils.AuthManager
 
 @Composable
@@ -70,6 +76,11 @@ fun HomeScreen(
     authManager: AuthManager,
     navigation: NavController
 ) {
+    val viewModel: MovieViewModel = viewModel {
+        MovieViewModel(MovieRepository())
+    }
+
+    val state by viewModel.state.collectAsState()
 
     var showDialog by remember { mutableStateOf(false) }
 
@@ -98,10 +109,8 @@ fun HomeScreen(
         Column(
             modifier = Modifier
                 .padding(it)
-                .padding(12.dp)
         ) {
-            Text(text = "Home")
-            HorizontalPagerHomeScreen()
+            HorizontalPagerHomeScreen(state.movies)
             Box() {
                 if (showDialog) {
                     LogoutDialog(onConfirmLogout = {
@@ -146,50 +155,52 @@ fun MyTopBar(
 ) {
     TopAppBar(
         title = {
-        Row(
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            if(user?.photoUrl != null) {
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(user?.photoUrl)
-                        .crossfade(true)
-                        .build(),
-                    contentDescription = "Imagen",
-                    placeholder = painterResource(id = R.drawable.profile),
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .size(40.dp))
-            } else {
-                Image(
-                    painter = painterResource(R.drawable.profile),
-                    contentDescription = "Foto de perfil por defecto",
-                    modifier = Modifier
-                        .padding(end = 8.dp)
-                        .size(40.dp)
-                        .clip(CircleShape)
-                )
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (user?.photoUrl != null) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(user?.photoUrl)
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = "Imagen",
+                        placeholder = painterResource(id = R.drawable.profile),
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .size(40.dp)
+                    )
+                } else {
+                    Image(
+                        painter = painterResource(R.drawable.profile),
+                        contentDescription = "Foto de perfil por defecto",
+                        modifier = Modifier
+                            .padding(end = 8.dp)
+                            .size(40.dp)
+                            .clip(CircleShape)
+                    )
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+                Column {
+                    Text(
+                        text = if (!user?.displayName.isNullOrEmpty()) "Hola ${user?.displayName}" else "Bienvenidx",
+                        fontSize = 20.sp,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Text(
+                        text = if (!user?.email.isNullOrEmpty()) "${user?.email}" else "Anónimo",
+                        fontSize = 12.sp,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
             }
-            Spacer(modifier = Modifier.width(10.dp))
-            Column {
-                Text(
-                    text = if(!user?.displayName.isNullOrEmpty()) "Hola ${user?.displayName}" else "Bienvenidx",
-                    fontSize = 20.sp,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Text(
-                    text = if(!user?.email.isNullOrEmpty()) "${user?.email}" else "Anónimo",
-                    fontSize = 12.sp,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis)
-            }
-        }
-    },
+        },
         actions = {
-            if ( user != null ){
+            if (user != null) {
                 IconButton(onClick = { onClickLogout() }) {
                     Icon(Icons.Outlined.ExitToApp, contentDescription = "Cerrar sesión")
                 }
@@ -200,19 +211,31 @@ fun MyTopBar(
             }
 
         }
-        
+
     )
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun HorizontalPagerHomeScreen(){
+fun HorizontalPagerHomeScreen(state: List<Movie>) {
 
-    val pagerState = rememberPagerState(pageCount = {3})
+    val pagerState = rememberPagerState(pageCount = { state.size })
 
-    Card {
-        HorizontalPager(state = pagerState, pageSize = PageSize.Fill) {
-
+    Card(modifier = Modifier.fillMaxSize(), shape = RectangleShape) {
+        HorizontalPager(
+            state = pagerState,
+            pageSize = PageSize.Fill,
+            modifier = Modifier.height(400.dp)
+        ) {
+            if (state.isEmpty()) {
+                Text(text = "is empty")
+            } else {
+                CarrouselItem(
+                    image = state[it].poster_path,
+                    title = state[it].title,
+                    description = state[it].overview
+                )
+            }
         }
         Row(
             Modifier
@@ -221,7 +244,7 @@ fun HorizontalPagerHomeScreen(){
                 .background(Color.Transparent),
             horizontalArrangement = Arrangement.Center
         ) {
-            repeat(3) { iteration ->
+            repeat(4) { iteration ->
                 val color =
                     if (pagerState.currentPage == iteration) White else LightGray
                 Box(
@@ -235,4 +258,38 @@ fun HorizontalPagerHomeScreen(){
     }
 }
 
+@Composable
+fun CarrouselItem(image: String, title: String, description: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(model = "https://image.tmdb.org/t/p/original/${image}"),
+            contentDescription = title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(8.dp)
+        ) {
+            Text(
+                text = title,
+                color = White,
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold,
+                )
+            Text(
+                text = description,
+                color = White,
+                fontSize = 12.sp,
+                maxLines = 2,
+                lineHeight = 12.sp
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+        }
+    }
+}
 
--- app/src/main/java/com/pop/moviemanagement/ui/MoviesScreen.kt
+++ app/src/main/java/com/pop/moviemanagement/ui/MoviesScreen.kt
@@ -1,15 +1,22 @@
 package com.pop.moviemanagement.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,43 +25,73 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import com.pop.moviemanagement.model.repository.MovieRepository
 import com.pop.moviemanagement.ui.navigation.TrackScreen
 import com.pop.moviemanagement.utils.AnalyticsManager
 
 @Composable
 fun MoviesScreen() {
-    
+
     val viewModel: MovieViewModel = viewModel {
         MovieViewModel(MovieRepository())
     }
-    
+
     val state by viewModel.state.collectAsState()
-    
+
     Scaffold(topBar = { MyTopBarScreens("Movies") }) {
         Column(modifier = Modifier.padding(it)) {
-            LazyColumn() {
-                items(state.movies){
-                    Text(text = it.title)
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2)
+            ) {
+                items(state.movies) { movie ->
+                    MovieItem(title = movie.title, image = movie.poster_path)
                 }
             }
         }
     }
 }
+
 @Composable
-fun MyTopBarScreens(title: String){
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .padding(12.dp)) {
-        Text(text = title, modifier = Modifier.align(Alignment.TopStart), fontWeight = FontWeight.Bold, fontSize = 25.sp)
-        Row(modifier = Modifier.align(Alignment.TopEnd), verticalAlignment = Alignment.CenterVertically) {
+fun MyTopBarScreens(title: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp)
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.align(Alignment.TopStart),
+            fontWeight = FontWeight.Bold,
+            fontSize = 25.sp
+        )
+        Row(
+            modifier = Modifier.align(Alignment.TopEnd),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Icon(Icons.Filled.AccountCircle, contentDescription = "search")
             Icon(Icons.Filled.Search, contentDescription = "search")
         }
     }
 }
+
+@Composable
+fun MovieItem(title: String, image: String) {
+    Card(shape = RectangleShape) {
+        AsyncImage(
+            modifier = Modifier.fillMaxSize(),
+            model = "https://image.tmdb.org/t/p/w500/${image}",
+            contentDescription = "image",
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+//"https://image.tmdb.org/t/p/w185/${movie.posterPath}"
